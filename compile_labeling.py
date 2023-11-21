ce62e04de0617df4e7a31e456f0efd2e94c38321
Poof <poofy38@gmail.com>
2023-11-20 20:08:16 -0600

    Labelbox titles

--- compile_labeling.py
+++ compile_labeling.py
@@ -5,13 +5,14 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 env = os.path.dirname(os.path.abspath(__file__))
 
+database_path = f'D:/DATA/CASBUSI/database/'
 
 
 def compile_images(csv_file, output_dir, images_per_row=4):
     # Load CSV file
     failed_cases = pd.read_csv(csv_file)
-    df = pd.read_csv(f"{env}/database/ImageData.csv")
-    image_dir = f"{env}/database/images/"
+    df = pd.read_csv(f"{database_path}/ImageData.csv")
+    image_dir = f"{database_path}/images/"
 
     # Additional setup for tracking image placements
     image_placement_tracker = []
@@ -31,11 +32,17 @@ def compile_images(csv_file, output_dir, images_per_row=4):
 
     # Iterate over each Accession_Number
     for acc_num in tqdm(top_20_percent['Accession_Number']):
-        
         true_label_images = df_label_true[df_label_true['Accession_Number'] == acc_num]
+
+        # Get prediction and true label for the header
+        case_info = failed_cases[failed_cases['Accession_Number'] == acc_num]
+        prediction_label = "Malignant" if case_info['Prediction'].iloc[0] > 0.5 else "Benign"
+        true_label = "Malignant" if case_info['True_Label'].iloc[0] > 0.5 else "Benign"
+        header_text = f"Prediction: {prediction_label} | True Label: {true_label}"
 
         # Load images
         loaded_images = [Image.open(os.path.join(image_dir, img_name)) for img_name in true_label_images['ImageName']]
+
 
         # Determine the size of the compiled image
         max_width = max(img.size[0] for img in loaded_images)
@@ -44,15 +51,31 @@ def compile_images(csv_file, output_dir, images_per_row=4):
         panel_width = max_width * images_per_row
         panel_height = max_height * total_rows
 
+        # Adjust panel height to include header
+        header_height = 150 
+        panel_height += header_height
+
         # Create a new blank image
         panel = Image.new('RGB', (panel_width, panel_height))
         draw = ImageDraw.Draw(panel)
-        font = ImageFont.truetype("arialbd.ttf", size=100)  # Adjust font size as needed
+        font = ImageFont.truetype("arialbd.ttf", size=80)
+
+        # Draw the header
+        draw.rectangle([0, 0, panel_width, header_height], fill=(0, 0, 0))  # Black bar at the top
+
+        # Use textbbox for text size
+        text_bbox = draw.textbbox((0, 0), header_text, font=font)
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height = text_bbox[3] - text_bbox[1]
+        text_x = (panel_width - text_width) // 2
+        text_y = (header_height - text_height) // 2
+        draw.text((text_x, text_y), header_text, fill=(255, 255, 255), font=font)
+
 
         # Paste images into the panel and add grid coordinates
         for i, img in enumerate(loaded_images):
             x = (i % images_per_row) * max_width
-            y = (i // images_per_row) * max_height
+            y = (i // images_per_row) * max_height + header_height  # Offset by header height
             panel.paste(img, (x, y))
 
             # Calculate coordinates for text
@@ -72,9 +95,9 @@ def compile_images(csv_file, output_dir, images_per_row=4):
         panel.save(os.path.join(output_dir, f'ACC_{int(acc_num)}.png'))
 
     # Save the image placement information to a CSV file
-    pd.DataFrame(image_placement_tracker).to_csv(f'{env}/database/LossLabelingReferences.csv', index=False)
+    pd.DataFrame(image_placement_tracker).to_csv(f'{database_path}/LossLabelingReferences.csv', index=False)
 
 # Usage
-output_dir = f"{env}/database/LossLabeling/"
+output_dir = f"{database_path}/LossLabeling/"
 os.makedirs(output_dir, exist_ok=True)
 compile_images(f"{env}/failed_cases.csv", output_dir)
