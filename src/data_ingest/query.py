9cd1b646e2c36f62c8a384226a2f819d0eb07db3
Poof <poofy38@gmail.com>
2025-12-04 18:33:25 -0600

    Loosened radiology query

--- src/data_ingest/query.py
+++ src/data_ingest/query.py
@@ -9,23 +9,6 @@ env = os.path.dirname(os.path.abspath(__file__))
 env = os.path.dirname(env)  # Go back one directory
 env = os.path.dirname(env)  # Go back one directory
 
-BREAST_FILTER = """'IMG3425','IMG3426','IMG10897','IMG3571','IMG3557','IMG3558','IMG4636','IMG4637','IMG1100','IMG3506','IMG3545',
-                  'IMG3508','IMG616','IMG3566','IMG1974','IMG3543','IMG3245','IMG3507','IMG3546','IMG3509','IMG1950','IMG3567',
-                  'IMG1975','IMG3544','IMG3246','IMG10829','IMG10900','IMG3333','IMG3373','IMG3376','IMG3374','IMG3375','IMG3518',
-                  'IMG3519','IMG3504','IMG3505','IMG3371','IMG3251','IMG3252','IMG10738','IMG600','IMG610','IMG3287','IMG3288',
-                  'IMG588','IMG612','IMG3283','IMG3285','IMG589','IMG611','IMG3284','IMG3286','IMG1983','IMG579','IMG1982',
-                  'IMG3524','IMG3523','IMG617','IMG1952','IMG3551','IMG3512','IMG3520','IMG3513','IMG3517','IMG3521','IMG10826',
-                  'IMG3428','IMG3430','IMG3432','IMG3429','IMG3431','IMG3433','IMG3540','IMG3539','IMG3538','IMG3584','IMG3572',
-                  'IMG3574','IMG3576','IMG3573','IMG3575','IMG3577','IMG3552','IMG3302','IMG3215','IMG605','IMG3221','IMG3570',
-                  'IMG608','IMG3427','IMG3569','IMG609','IMG3222','IMG3568','IMG10828','IMG10902','IMG10808','IMG3382','IMG621',
-                  'IMG3388','IMG3381','IMG3387','IMG10832','IMG1972','IMG1976','IMG3255','IMG1973','IMG1977','IMG3256','IMG10833',
-                  'IMG3207','IMG2360','IMG3322','IMG3240','IMG3265','IMG3315','IMG3233','IMG3341','IMG3342','IMG3326','IMG3329',
-                  'IMG3330','IMG3241','IMG3248','IMG3249','IMG3308','IMG3211','IMG4225','IMG1073','IMG4009','IMG4115','IMG4159',
-                  'IMG3503','IMG3502','IMG4503','IMG3247','IMG4585','IMG5967','IMG10000','IMG3788','IMG1045','IMG1143','IMG1144',
-                  'IMG10769','IMG1069','IMG3510','IMG1971','IMG3511','IMG1970','IMG4852','IMG1044','IMG1142','IMG1639','IMG3979',
-                  'IMG4957','IMG4959','IMG1043','IMG1141','IMG1638','IMG3607','IMG2442','IMG13019','IMG13020','CMP021','CMP026',
-                  'IMG10823','IMG4307','IMG4838','IMG4839','IMG4765','IMG4897','IMG4898','IMG4899'"""
-
 def get_radiology_data(limit=None):
     """
     Get radiology data for breast imaging studies, ensuring all relevant accessions
@@ -41,27 +24,24 @@ def get_radiology_data(limit=None):
     print("Initializing BigQuery client for radiology data...")
     client = bigquery.Client()
     
+    # Build the limit clause for the CTE
+    limit_clause = f"LIMIT {limit}" if limit is not None else ""
+    
     query = f"""
-    -- First identify patients with breast imaging studies and at least one US modality
-    -- excluding patients with US_CORE_BIRTHSEX = 'M'
-    WITH breast_imaging_patients AS (
+    -- First identify ALL patients with US modality (excluding males)
+    -- NO procedure code filtering - just US modality
+    WITH us_imaging_patients AS (
       SELECT DISTINCT PAT_PATIENT.CLINIC_NUMBER AS PATIENT_ID
       FROM `ml-mps-adl-intfhr-phi-p-3b6e.phi_secondary_use_fhir_clinicnumber_us_p.ImagingStudy` imaging
       INNER JOIN `ml-mps-adl-intfhr-phi-p-3b6e.phi_secondary_use_fhir_clinicnumber_us_p.Patient` PAT_PATIENT 
         ON (imaging.clinic_number = PAT_PATIENT.clinic_number)
       INNER JOIN `ml-mps-adl-intudp-phi-p-d5cb.phi_udpwh_etl_us_p.FACT_RADIOLOGY` RAD_FACT_RADIOLOGY 
         ON (imaging.ACCESSION_IDENTIFIER_VALUE = RAD_FACT_RADIOLOGY.ACCESSION_NBR)
-      WHERE procedure_code_coding_code IN ({BREAST_FILTER})
-        AND RAD_FACT_RADIOLOGY.SERVICE_MODALITY_CODE = 'US'
+      WHERE RAD_FACT_RADIOLOGY.SERVICE_MODALITY_CODE = 'US'
         AND PAT_PATIENT.US_CORE_BIRTHSEX != 'M'
-    """
-    
-    if limit is not None:
-        query += f"\nLIMIT {limit}\n"
-        
-    query += f"""
+      {limit_clause}
     )
-    -- Then get all BREAST radiology data for these patients
+    -- Then get radiology data for these patients with BREAST test descriptions
     SELECT DISTINCT 
       PAT_PATIENT.CLINIC_NUMBER AS PATIENT_ID,
       RAD_FACT_RADIOLOGY.ACCESSION_NBR AS ACCESSION_NUMBER,
@@ -83,17 +63,17 @@ def get_radiology_data(limit=None):
       PAT_DIM_PATIENT.PATIENT_RACE_NAME AS RACE,
       DATE_DIFF(EXTRACT(DATE FROM RAD_FACT_RADIOLOGY.RADIOLOGY_DTM), PAT_DIM_PATIENT.PATIENT_BIRTH_DATE, YEAR) - 
         IF(EXTRACT(MONTH FROM PAT_DIM_PATIENT.PATIENT_BIRTH_DATE)*100 + EXTRACT(DAY FROM PAT_DIM_PATIENT.PATIENT_BIRTH_DATE) > 
-           EXTRACT(MONTH FROM RAD_FACT_RADIOLOGY.RADIOLOGY_DTM)*100 + EXTRACT(DAY FROM RAD_FACT_RADIOLOGY.RADIOLOGY_DTM),1,0) AS AGE_AT_EVENT,
+          EXTRACT(MONTH FROM RAD_FACT_RADIOLOGY.RADIOLOGY_DTM)*100 + EXTRACT(DAY FROM RAD_FACT_RADIOLOGY.RADIOLOGY_DTM),1,0) AS AGE_AT_EVENT,
       PAT_DIM_PATIENT.PATIENT_BIRTH_DATE AS BIRTH_DATE,
       -- Added breast-specific pathology fields
       breast_results.A1_PATHOLOGY_TXT,
       breast_results.A1_PATHOLOGY_CATEGORY_DESC,
       breast_results.A2_PATHOLGY_TXT,
       breast_results.A2_PATHOLOGY_CATEGORY_DESC
-    FROM breast_imaging_patients
+    FROM us_imaging_patients
     INNER JOIN 
       `ml-mps-adl-intfhr-phi-p-3b6e.phi_secondary_use_fhir_clinicnumber_us_p.Patient` PAT_PATIENT 
-      ON breast_imaging_patients.PATIENT_ID = PAT_PATIENT.CLINIC_NUMBER
+      ON us_imaging_patients.PATIENT_ID = PAT_PATIENT.CLINIC_NUMBER
     INNER JOIN 
       `ml-mps-adl-intudp-phi-p-d5cb.phi_udpwh_etl_us_p.DIM_PATIENT` PAT_DIM_PATIENT
       ON PAT_PATIENT.CLINIC_NUMBER = PAT_DIM_PATIENT.PATIENT_CLINIC_NUMBER
@@ -109,7 +89,6 @@ def get_radiology_data(limit=None):
     INNER JOIN 
       `ml-mps-adl-intudp-phi-p-d5cb.phi_udpwh_etl_us_p.DIM_RADIOLOGY_TEST_NAME` RADTEST_DIM_RADIOLOGY_TEST_NAME 
       ON (RAD_FACT_RADIOLOGY.RADIOLOGY_TEST_NAME_DK = RADTEST_DIM_RADIOLOGY_TEST_NAME.RADIOLOGY_TEST_NAME_DK)
-    -- Join to get breast-specific pathology data
     LEFT JOIN 
       `ml-mps-adl-intudp-phi-p-d5cb.phi_rad_udpwh_us_p.DIM_RADIOLOGY_EXAM` rad_exam
       ON (RAD_FACT_RADIOLOGY.ACCESSION_NBR = rad_exam.ACCESSION_NBR_ID)
